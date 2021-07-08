52739f64967868096df46cbf662833a530b9ea0f
swkidd <kiddsoftware@gmail.com>
2021-07-08 16:22:30 +0900

    remove comments

--- index.js
+++ index.js
@@ -31,17 +31,17 @@ exports.handler = async(event, context) => {
       }))(claims)
 
       switch (event.routeKey) {
-        case "DELETE /markers/{id}":
-          await dynamo
-            .delete({
-              TableName: MARKER_DB,
-              Key: {
-                id: event.pathParameters.id
-              }
-            })
-            .promise();
-          body = `Deleted item ${event.pathParameters.id}`;
-          break;
+        // case "DELETE /markers/{id}":
+        //   await dynamo
+        //     .delete({
+        //       TableName: MARKER_DB,
+        //       Key: {
+        //         id: event.pathParameters.id
+        //       }
+        //     })
+        //     .promise();
+        //   body = `Deleted item ${event.pathParameters.id}`;
+        //   break;
         case "GET /markers":
           params = {
             TableName: MARKER_DB,
@@ -61,54 +61,6 @@ exports.handler = async(event, context) => {
               }
             })
             .promise();
-          break;
-        case "PUT /comments":
-          requestJSON = JSON.parse(event.body);
-          let mems = []
-          if (requestJSON.mems) {
-            mems = requestJSON.mems.map(mem => {
-              return { id: uuidv4(), ...mem }
-            })
-          }
-          // id = requestJSON.id || uuidv4()
-          id = uuidv4()
-          item = {
-            id,
-            order: requestJSON.order,
-            links: requestJSON.links,
-            front: requestJSON.front,
-            back: requestJSON.back,
-            markerId: requestJSON.markerId,
-            imageIndex: requestJSON.imageIndex,
-            mems,
-            creator,
-            polygons: requestJSON.polygons,
-            width: requestJSON.width,
-          }
-          await dynamo
-            .put({
-              TableName: COMMENT_DB,
-              Item: item
-            })
-            .promise();
-          body = item;
-          break;
-        case "GET /comments/{id}":
-          // id is marker id
-
-          params = {
-            TableName: COMMENT_DB,
-            FilterExpression: 'markerId = :markerId',
-            ExpressionAttributeValues: {
-              ':markerId': event.pathParameters.id
-            },
-          };
-
-          body = await dynamo.scan(params).promise();
-
-          break;
-        case "GET /comments":
-          body = await dynamo.scan({ TableName: COMMENT_DB }).promise();
           break;
         case "GET /mem":
           body = await dynamo.scan({ TableName: MEM_DB }).promise();
